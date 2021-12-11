114b4c4752a5be3605fb8134a7b4a4824e926639
Michael <mvalentine65@gmail.com>
2021-12-11 15:28:56 -0500

    Seperates cdll creation and use. This greatly improves performance during repeated runs.

--- python/ctype_test/cbrot.py
+++ python/ctype_test/cbrot.py
@@ -1,10 +1,26 @@
 import ctypes
-import numpy as np
 
-def cbrot(re: float, im: float, max_iterations=200):
-    so_file = './mandlebrot.so'
-    mandlebrot_so = ctypes.CDLL(so_file)
+
+def make_cdll_from_so(so_path: str):
+    mandlebrot_so = ctypes.CDLL(so_path)
     mandlebrot_function = mandlebrot_so.mandlebrot
     mandlebrot_function.argtypes = (ctypes.c_double, ctypes.c_double, ctypes.c_int)
+    return mandlebrot_function
+
+
+def call_mandlebrot_cdll(re: float, im: float, cdll, max_iterations=200) -> int:
+    iterations = cdll(re, im, max_iterations)
+    return iterations
+
+def cbrot(re: float, im: float, max_iterations=200) -> int:
+    so_file = './mandlebrot.so'
+    mandlebrot_function = make_cdll_from_so(so_file)
     iterations = mandlebrot_function(re, im, max_iterations)
     return iterations
+
+if __name__ == '__main__':
+    so_file = './mandlebrot.so'
+    cdll = make_cdll_from_so(so_file)
+    print(call_mandlebrot_cdll(0,0,cdll))
+    
+    
---
+++ python/ctype_test/compare_cdll_time.py
@@ -0,0 +1,23 @@
+from cbrot import *
+from sys import argv
+import time
+
+
+runs = int(argv[1])
+
+start_1 = time.time()
+for i in range(runs):
+    iteration = cbrot(0.0, 0.0, 200)
+end_1 = time.time()
+total = end_1 - start_1
+print(f'cbrot time: {total}')
+print(f'mean cbrot time for {runs} runs: {total/runs}')
+
+start_2 = time.time()
+cdll = make_cdll_from_so('./mandlebrot.so')
+for i in range(runs):
+    iteration = call_mandlebrot_cdll(0.0, 0.0, cdll, max_iterations=200)
+end_2 = time.time()
+total = end_2 - start_2
+print(f'seperated time: {total}')
+print(f'mean seperated time for {runs} runs: {total/runs}')
