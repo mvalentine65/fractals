7bad09f6193958e8175ff5b4d3a47e7d84f3d63f
Michael <mvalentine65@gmail.com>
2021-12-14 01:43:06 -0500

    Adds a basic visualizer. Creaters helper subdirectory.
    This commit finishes the basic boilerplate code. The next major steps are
    implementing multi-processor support and creating the endpoints.

---
+++ python/helpers/data.py
@@ -0,0 +1,49 @@
+"""
+Utility functions for making the data matrix.
+"""
+import numpy as np
+
+
+def find_length(step: float) -> int:
+    """Finds the number of steps needed to cover -2 to 2, the entire length of
+    the mandlebrot graph. Takes the increment value as a float, returns the result
+    as a int."""
+    MAX_DISTANCE = 4
+    length = int(MAX_DISTANCE/step) + 1
+    return length
+    
+
+
+def make_matrix(step=0.025) -> np.ndarray:
+    """Makes a 2d numpy array. Each value represents one point of data
+    in the final graph."""
+    length = find_length(step)
+    shape = (length, length)
+    matrix = np.zeros(shape)
+    return matrix
+
+
+def find_delta(step: float, index: int) -> float:
+    """Multiples the step value by the index value to find the total change."""
+    result = step * index
+    return result
+
+     
+def find_real_component(step: float, index: int) -> float:
+    """Finds the real component of the complex number C. Takes the step
+    and index in the numpy matrix as arguments. Returns the real component
+    as a float."""
+    X_START = -2
+    delta = find_delta(step, index)
+    result = X_START + delta
+    return result
+
+    
+def find_imaginary_component(step: float, index: int) -> float:
+    """Finds the imaginary component of the complex number C. Takes the step
+    and index in the numpy matrix as arguments. Returns the imaginary component
+    as a float."""
+    Y_START = 2
+    delta = find_delta(step, index)
+    result = Y_START - delta
+    return result
--- python/picture.py
+++ python/picture.py
@@ -4,11 +4,19 @@ Visualizes the mandlebrot set with the given width and height. Requires cbrot.py
 import argparse
 from ctype_test.cbrot import make_cdll_from_so
 from ctype_test.cbrot import call_mandlebrot_cdll
+from helpers import data
 from matplotlib import pyplot as plt
+import numpy as np
 from sys import argv
 
 
-def get_mandlebrot():
+def get_data_matrix(step: float) -> np.matrix:
+    """Returns a 2d numpy array of length 4//step."""
+    matrix = data.make_matrix(step)
+    return matrix
+
+
+def get_mandlebrot_function():
     """Getter for mandlebrot function."""
     mandlebrot_function = make_cdll_from_so('ctype_test/mandlebrot.so')
     return mandlebrot_function
@@ -17,20 +25,55 @@ def get_mandlebrot():
 def make_plot():
     """Returns a pyplot plot object"""
     plot = plt.figure()
-    
+    return plot 
 
+
+def find_value(function, real: float, imaginary: float, max_iterations: int) -> float:
+    """Runs the provided function with the provided arguments. Returns the
+    (found iterations / max_iterations) % 1 as a float."""
+    value = function(real, imaginary, max_iterations)
+    value = value / max_iterations
+    value = value % 1
+    return value
+
+
+def fill_graph(matrix: np.matrix, step: float, function, max_iterations: int) -> None:
+    """Iterates over the given numpy matrix and replaces the existing
+    values with the number of iterations at that x,y coodinate."""
+    # x starts at -2, ends at 2
+    # y starts at 2i, ends at -2i
+    # x = -2 + step*index
+    # y = 2 - step*index
+    length = len(matrix)
+    for y in range(length):
+        imaginary_component = data.find_imaginary_component(step, y)
+        for x in range(length):
+            real_component = data.find_real_component(step, x)
+            value = function(real_component, imaginary_component, max_iterations)
+            matrix[y][x] = value
 
 
 def main(argv):
     parser = argparse.ArgumentParser(
                 description="Visualize the mandlebrot set with the given width and height")
-    #parser.add_argument('--width', type=int, default=400,
-                        #help='Window width in pixels.')
-    #parser.add_argument('--height', type=int, default=400,
-                        #help='Window height in pixels.')
+    parser.add_argument('--width', type=int, default=8,
+                        help='Width in inches.')
+    parser.add_argument('--height', type=int, default=8,
+                        help='Height in inches.')
+    parser.add_argument('--step', type=float, default=0.02,
+                        help="Increment for x and y values.")
+    parser.add_argument('--max-iterations', type=int, default=200,
+                        help="""Maximum number of iterations to try before a
+                        starting value is considered in the set.""")
     args = parser.parse_args()
-    mandlebrot = get_mandlebrot()
-    graph = make_plot()
+    mandlebrot_function = get_mandlebrot_function()
+    data = get_data_matrix(args.step)
+    fill_graph(data, args.step, mandlebrot_function, args.max_iterations)
+    #graph = make_plot()
+    plt.imshow(data)
+    plt.show()
+    print(data)
+
 
 if __name__ == '__main__':
     main(argv)
